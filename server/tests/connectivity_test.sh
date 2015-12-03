8330d7372416f08c82bf7db593e7b7a4ebbded37
Pablo Rodiz <pablorodiz@gmail.com>
2015-12-03 13:46:17 -0500

    added first test for checking server launches correctly

---
+++ server/tests/connectivity_test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Launch server
+../Debug/server > connectivity_test.out 2>connectivity_test.err &
+PID=`pidof ../Debug/server`
+sleep 2
+#Send some text to the server
+echo "hello" | nc -q 2 127.0.0.1 6423
+#Terminate server
+disown $PID
+kill $PID
+#Check server response
+if [ -z "`diff connectivity_test.ok connectivity_test.out`" ] && [ -f `cat connectivity_test.err` ] 
+  then
+    echo "Connectivity test OK"
+    rm connectivity_test.out connectivity_test.err
+  else
+    echo "Connectivity test FAILED"
+fi
