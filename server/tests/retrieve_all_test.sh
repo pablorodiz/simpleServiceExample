90f0fe9cf01eb39ed632f901607675d559e03bac
Pablo Rodiz <pablorodiz@gmail.com>
2015-12-04 19:03:18 -0500

    Completed retrieve all test to check server response

--- server/tests/retrieve_all_test.sh
+++ server/tests/retrieve_all_test.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
+cp retrieve_all_test.database.txt database.txt
 #Launch server
 ../Debug/server > retrieve_all_test.out 2>retrieve_all_test.err &
 PID=`pidof ../Debug/server`
 sleep 2
 #Send some text to the server
-cat retrieve_all_test.in | nc -q 2 127.0.0.1 6423
+cat retrieve_all_test.in | nc -q 2 127.0.0.1 6423 > retrieve_all_test.client
 #Terminate server
 disown $PID
 kill $PID
 #Check server response
-#if [ -z "`diff retrieve_all_test.ok retrieve_all_test.out`" ] && [ -f `cat retrieve_all_test.err` ] 
-#  then
-#    echo "Retrieve All test OK"
-#    rm retrieve_all_test.out retrieve_all_test.err
-#  else
-#    echo "Retrieve All test FAILED"
-#fi
+if [ -z "`diff retrieve_all_test.out.ok retrieve_all_test.out`" ] && [ -z "`diff retrieve_all_test.client.ok retrieve_all_test.client`" ] && [ -f `cat retrieve_all_test.err` ] 
+  then
+    echo "Retrieve All test OK"
+    rm retrieve_all_test.out retrieve_all_test.err retrieve_all_test.client
+  else
+    echo "Retrieve All test FAILED"
+fi
